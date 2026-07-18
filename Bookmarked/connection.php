20c6e045cff9a36bfdba0bb393709c6627ed14f8
LeinnaSophia <malay.ls0601@gmail.com>
2026-07-19 00:06:23 +0800

    updated the file

--- Bookmarked/connection.php
+++ Bookmarked/connection.php
@@ -1,12 +1,12 @@
 <?php 
     $host = "localhost";
     $dbuser = "root";
-    $dbpass = "L3tsD0SQL1!";
+    $dbpass = "L3tsD0SQL1!"; //change this password to the root password used in your device
     $dbname = "bookmarked";
 
     $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
 
     if (mysqli_connect_errno()){
-        echo "Connection failed: " . msqli_connect_error();
+        echo "Connection failed: " . mysqli_connect_error();
     }
 ?>
